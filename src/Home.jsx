807e95c911a9e74129ead5d1ab2201e9b02d43c7
Joao Lino <linusmannuel@gmail.com>
2022-09-16 14:35:56 +0100

    feat(index,Home): Implemented the outputting Lists using the map method

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,19 +2,19 @@ import './index.css';
 import { useState } from 'react';
 
 const Home = () => {
-  const [name, setName] = useState('Linus');
-  const [age, setAge] = useState(20);
-
-  const handleClick = () => {
-    setName('Mario')
-    setAge(60);
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
   return (
     <div className="b-home">
-      <h1 className="c-title">Homepage</h1>
-      <button onClick={handleClick}>Click me</button>
-      <p> { name } is { age } years old</p>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2 className="c-title--2">{ blog.title}</h2>
+          <p className="c-text">Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
