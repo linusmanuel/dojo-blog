aeac3d8b1f5b7245f2d64bbdd438fa80b398fefa
Joao Lino <linusmannuel@gmail.com>
2022-09-16 10:50:12 +0100

    feat(Home): Added handleClick events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,19 @@
 import './index.css';
 
+const handleClick = () => {
+  alert('Hello Ninja')
+}
+
+const handleClickAgain = ( name ) => {
+  alert('Hello ' + name)
+}
+
 const Home = () => {
   return (
     <div className="b-home">
       <h1 className="c-title">Homepage</h1>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain('Linus')}>Click me Again</button>
     </div>
   );
 }
