b7b511d2bf47dd9898ab7143ee23dee7a0945a42
Joao Lino <linusmannuel@gmail.com>
2022-09-16 08:58:41 +0100

    feat(app): Created the title component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="c-title">App Component</h1>
+    </div>
+  );
+}
+
+export default App;
