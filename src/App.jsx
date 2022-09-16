5e82ec22b26ecd8d5043f08c3282e12e05a831c8
Joao Lino <linusmannuel@gmail.com>
2022-09-16 09:10:19 +0100

    feat(app): Dynamic values in templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const liked =50;
+
   return (
     <div className="App">
-      <h1 className="c-title">App Component</h1>
+      <h1 className="c-title">{ title }</h1>
+      <p className="c-text">Liked { liked } times</p>
     </div>
   );
 }
